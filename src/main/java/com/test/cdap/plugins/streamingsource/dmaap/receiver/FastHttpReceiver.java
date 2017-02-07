383a7b1168f1065ccc6091f8545c8a9c8743973d
CDAP SDK Service Account <cdap@cdap-standalone-vm-3.5.1.cask.co>
2017-02-07 15:17:30 +0000

    Added JSON Parser

--- src/main/java/com/test/cdap/plugins/streamingsource/dmaap/receiver/FastHttpReceiver.java
+++ src/main/java/com/test/cdap/plugins/streamingsource/dmaap/receiver/FastHttpReceiver.java
@@ -42,17 +42,23 @@ public class FastHttpReceiver implements CustomReceiver {
             public void run() {
                 AtomicInteger serialNumber = new AtomicInteger(200);
 
-                while (!abstractReceiver.isStopped()) {
+
+                //while (!abstractReceiver.isStopped()) {
+                for (int i = 0; i < 5; i++) {
                     try {
                         TimeUnit.MILLISECONDS.sleep(1000);
-
-                        String msg = "Message " + Integer.toString(serialNumber.incrementAndGet()) + "\r\n";
-                        StructuredRecord recordForNow = StructuredRecord.builder(OUTPUT_SCHEMA).
+                        LOG.error("Version 2");
+                        LOG.error("!!!!!!!!!!!!! Emitting message !!!!!!!!!!!!!!");
+                        abstractReceiver.store(StructuredRecord.builder(OUTPUT_SCHEMA).
                                 set("MESSAGE_NUM", Long.toString(System.currentTimeMillis())).
-                                set("MESSAGE",  msg)
-                                .build();
+                                set("MESSAGE", getGoodMessage())
+                                .build());
 
-                        abstractReceiver.store(recordForNow);
+                        TimeUnit.MILLISECONDS.sleep(1000);
+                        abstractReceiver.store(StructuredRecord.builder(OUTPUT_SCHEMA).
+                                set("MESSAGE_NUM", Long.toString(System.currentTimeMillis())).
+                                set("MESSAGE", getBadMessage())
+                                .build());
 
                     } catch (Exception e) {
                         LOG.error("Error getting content from {}.", e);
@@ -65,6 +71,33 @@ public class FastHttpReceiver implements CustomReceiver {
                 super.interrupt();
             }
         }.start();
+    }
+
+    String getGoodMessage() {
+        return "{\"cef\":{\"event\":{\"id\":\"id-1\",\"city\":\"Princeton\",\"state\":\"NJ\",\"country\":\"USA\"}}}";
+        /*return "{\n" +
+                "  \"cef\": {\n" +
+                "    \"event\": {\n" +
+                "      \"id\": \"id-1\",\n" +
+                "      \"city\": \"Princeton\",\n" +
+                "      \"state\": \"NJ\",\n" +
+                "      \"country\": \"USA\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";*/
+
+    }
+
+    String getBadMessage() {
+        return "{\"cef\":{\"event\":{\"id\":\"id-1\",\"country\":\"USA\"}}}";
+        /*return "{\n" +
+                "  \"cef\": {\n" +
+                "    \"event\": {\n" +
+                "      \"id\": \"id-1\",\n" +
+                "      \"country\": \"USA\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";*/
     }
 
     @Override
