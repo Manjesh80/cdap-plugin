896f6d0ff2578fa15fcc407dbdab92f746d1e198
CDAP SDK Service Account <cdap@cdap-standalone-vm-3.5.1.cask.co>
2017-02-16 20:34:22 +0000

    Added Keyvalue table

--- custom-module/src/main/java/com/test/cdap/plugins/compute/DataWindower.java
+++ custom-module/src/main/java/com/test/cdap/plugins/compute/DataWindower.java
@@ -36,13 +36,37 @@ import static akka.remote.security.provider.AkkaProvider.get;
 public class DataWindower extends SparkCompute<StructuredRecord, StructuredRecord> {
 
     private static final Logger LOG = LoggerFactory.getLogger(DataWindower.class);
-
     public static final String PLUGIN_NAME = "DataWindower";
     private DataWindowerConfig dataWindowerConfig;
-    private List<Schema.Field> fields;
+    //private List<Schema.Field> fields;
     // Output Schema that specifies the fields of JSON object
-    private Schema outSchema;
-    private Table messageHistoryTable;
+    //private Schema outSchema;
+    //private Table messageHistoryTable;
+
+    public static class DataWindowerConfig extends PluginConfig {
+        private final Logger LOG = LoggerFactory.getLogger(DataWindowerConfig.class);
+
+        @Description("Field to be used to apply window operation.")
+        public final String messageField;
+
+        @Description("Window duration to qualify a message as stale or new")
+        @Macro
+        public final Integer windowDuration;
+
+        @Description("Field to indicate the status of the message")
+        public final String messageStatus;
+
+        @Name("schema")
+        @Description("Output schema")
+        public final String schema;
+
+        public DataWindowerConfig(String messageField, Integer windowDuration, String messageStatus, String schema) {
+            this.messageField = messageField;
+            this.windowDuration = windowDuration;
+            this.messageStatus = messageStatus;
+            this.schema = schema;
+        }
+    }
 
     public DataWindower(DataWindowerConfig dataWindowerConfig) {
         this.dataWindowerConfig = dataWindowerConfig;
@@ -67,7 +91,7 @@ public class DataWindower extends SparkCompute<StructuredRecord, StructuredRecor
         try {
             Schema outputSchema = Schema.parseJson(dataWindowerConfig.schema);
             pipelineConfigurer.getStageConfigurer().setOutputSchema(outputSchema);
-            fields = outputSchema.getFields();
+            //fields = outputSchema.getFields();
 
             if (outputSchema.getField(dataWindowerConfig.messageStatus) == null) {
                 throw new IllegalArgumentException(String.format("Field %s is not present in output schema", dataWindowerConfig.messageStatus));
@@ -89,51 +113,70 @@ public class DataWindower extends SparkCompute<StructuredRecord, StructuredRecor
     @Override
     public void initialize(SparkExecutionPluginContext context) throws Exception {
         super.initialize(context);
-
-        messageHistoryTable = context.getDataset("messageHistory");
-
-        try {
+        /*try {
             outSchema = Schema.parseJson(dataWindowerConfig.schema);
             fields = outSchema.getFields();
         } catch (IOException e) {
             throw new IllegalArgumentException("Output Schema specified is not a valid JSON. Please check the Schema JSON.");
-        }
+        }*/
     }
 
     @Override
     public JavaRDD<StructuredRecord> transform(SparkExecutionPluginContext context,
                                                JavaRDD<StructuredRecord> javaRDD) throws Exception {
-        JavaRDD<StructuredRecord> convertedRDD = javaRDD.map(new DataWindowMapper(dataWindowerConfig,
-                outSchema, messageHistoryTable));
+
+        final Table messageHistoryTable = context.getDataset("messageHistory");
+        final Schema outputSchema = Schema.parseJson(dataWindowerConfig.schema);
+        JavaRDD<StructuredRecord> convertedRDD = javaRDD.map(new Function<StructuredRecord, StructuredRecord>() {
+            @Override
+            public StructuredRecord call(StructuredRecord structuredRecord) throws Exception {
+                String message = structuredRecord.get(dataWindowerConfig.messageField);
+                LOG.error("Message received ==> " + message);
+
+                String messageStatus = "Unknown";
+                boolean upsertMessage = true;
+                long currentTime = System.currentTimeMillis();
+
+                Row messageRow = messageHistoryTable.get(new Get(message));
+                if (!messageRow.isEmpty()) {
+
+                    long lastNotified = messageRow.getLong("lastNotified", -1);
+
+                    if (lastNotified != -1 && lastNotified >= currentTime) {
+                        long notificationGap = TimeUnit.MILLISECONDS.toSeconds(lastNotified - currentTime);
+                        if (notificationGap > dataWindowerConfig.windowDuration) {
+                            messageStatus = "NEW";
+                        } else
+                            messageStatus = "STALE";
+                        upsertMessage = false;
+                    }
+                } else {
+                    messageStatus = "NEW";
+                }
+
+                if (upsertMessage) {
+                    messageHistoryTable.put(new Put(message).add("lastNotified", currentTime));
+                }
+
+
+                StructuredRecord.Builder builder = StructuredRecord.builder(outputSchema);
+                for (Schema.Field field : outputSchema.getFields()) {
+                    if (structuredRecord.get(field.getName()) != null) {
+                        builder.set(field.getName(), structuredRecord.get(field.getName()));
+                    }
+                }
+                builder.set(dataWindowerConfig.messageStatus, "STALE");
+                return builder.build();
+            }
+        });
+        /*JavaRDD<StructuredRecord> convertedRDD = javaRDD.map(new DataWindowMapper(dataWindowerConfig,
+                outSchema, messageHistoryTable)); */
         return convertedRDD;
     }
 }
 
-class DataWindowerConfig extends PluginConfig {
-    private static final Logger LOG = LoggerFactory.getLogger(DataWindowerConfig.class);
 
-    @Description("Field to be used to apply window operation.")
-    public final String messageField;
-
-    @Description("Window duration to qualify a message as stale or new")
-    @Macro
-    public final Integer windowDuration;
-
-    @Description("Field to indicate the status of the message")
-    public final String messageStatus;
-
-    @Name("schema")
-    @Description("Output schema")
-    public final String schema;
-
-    public DataWindowerConfig(String messageField, Integer windowDuration, String messageStatus, String schema) {
-        this.messageField = messageField;
-        this.windowDuration = windowDuration;
-        this.messageStatus = messageStatus;
-        this.schema = schema;
-    }
-}
-
+/*
 
 class DataWindowMapper implements Function<StructuredRecord, StructuredRecord> {
 
@@ -194,6 +237,7 @@ class DataWindowMapper implements Function<StructuredRecord, StructuredRecord> {
 
 
 
+*/
 
 
 
